2c9a52572a27e4d704034437a373d331c3d3886d
igoodwinTechtonic <ian.goodwin@techtonic.com>
2020-12-28 16:09:39 -0600

    Learning Vue

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+// import { Vue } from 'vue';
+
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'Socks are most often worn on your feet, but sometimes also make great puppets.'
+    }
+  }
+})
